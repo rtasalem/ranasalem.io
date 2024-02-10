b9aa05f8a80c62aa4aa7cf14ab4c66d8bba7c5ed
Rana Salem <ranatasalem@gmail.com>
2024-02-10 00:49:59 +0000

    add placeholder for header & footer links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,10 @@
 const Footer: React.FC = () => {
     return(
         <div>
-            <h1>footer</h1>
+            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+                <a href="https://github.com/rtasalem" className="-mx3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900">github</a>
+                <a href="https://www.linkedin.com/in/ranatasalem/" className="-mx3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900">linkedin</a>
+            </nav>
         </div>
     )
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,13 @@
 const Header: React.FC = () => {
     return (
         <div>
-            <h1>header</h1>
+            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+                <a href="/" className="-mx3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900">rsalem.io</a>
+                <a href="/about" className="-mx3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900">About</a>
+                <a href="/blog" className="-mx3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900">Ramblings</a>
+                <a href="/projects" className="-mx3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900">Projects</a>
+                <a href="/contact" className="-mx3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900">Contact</a>
+            </nav>
         </div>
     )
 }
