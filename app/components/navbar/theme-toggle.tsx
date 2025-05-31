90ef6f0b1d304a40e3dcc72f6f623542269b6085
Rana Salem <ranatasalem@gmail.com>
2025-05-31 15:12:41 +0100

    wip

--- app/components/navbar/theme-toggle.tsx
+++ app/components/navbar/theme-toggle.tsx
@@ -1,37 +1,75 @@
+"use client"
+
+import { useState } from "react"
+
 export function ThemeToggle() {
+  const [theme, setTheme] = useState("light")
+
+  function handleThemeChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const selectedTheme = event.target.value
+    setTheme(selectedTheme)
+    document.documentElement.setAttribute("data-theme", selectedTheme)
+  }
+
+  function renderThemeIcon() {
+    if (theme === "light") {
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+          <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+        </svg>
+      )
+    } else if (theme === "black") {
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+          <path fillRule="evenodd" d="M7.455 2.004a.75.75 0 0 1 .26.77 7 7 0 0 0 9.958 7.967.75.75 0 0 1 1.067.853A8.5 8.5 0 1 1 6.647 1.921a.75.75 0 0 1 .808.083Z" clipRule="evenodd" />
+        </svg>
+      )
+    } else if (theme === "rana") {
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
+        </svg>
+      )
+    }
+  }
+
   return (
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button" className="btn btn-ghost m-1">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-          <path d="M10 2a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 10 2ZM10 15a.75.75 0 0 1 .75.75v1.5a.75.75 0 0 1-1.5 0v-1.5A.75.75 0 0 1 10 15ZM10 7a3 3 0 1 0 0 6 3 3 0 0 0 0-6ZM15.657 5.404a.75.75 0 1 0-1.06-1.06l-1.061 1.06a.75.75 0 0 0 1.06 1.06l1.06-1.06ZM6.464 14.596a.75.75 0 1 0-1.06-1.06l-1.06 1.06a.75.75 0 0 0 1.06 1.06l1.06-1.06ZM18 10a.75.75 0 0 1-.75.75h-1.5a.75.75 0 0 1 0-1.5h1.5A.75.75 0 0 1 18 10ZM5 10a.75.75 0 0 1-.75.75h-1.5a.75.75 0 0 1 0-1.5h1.5A.75.75 0 0 1 5 10ZM14.596 15.657a.75.75 0 0 0 1.06-1.06l-1.06-1.061a.75.75 0 1 0-1.06 1.06l1.06 1.06ZM5.404 6.464a.75.75 0 0 0 1.06-1.06l-1.06-1.06a.75.75 0 1 0-1.061 1.06l1.06 1.06Z" />
-        </svg>
+        {renderThemeIcon()}
       </div>
-      <ul tabIndex={0} className="dropdown-content bg-base-300 rounded-box z-10 w-32 p-2 shadow-2xl">
+      <ul tabIndex={0} className="dropdown-content bg-base-300 rounded-box z-10 w-28 p-2 shadow-2xl">
         <li>
           <input
             type="radio"
             name="theme-dropdown"
-            className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-end"
-            aria-label="Light mode"
-            value="lemonade"
+            className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+            aria-label="Light"
+            value="light"
+            onChange={handleThemeChange}
+            checked={theme === "light"}
           />
         </li>
         <li>
           <input
             type="radio"
             name="theme-dropdown"
-            className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-end"
-            aria-label="Dark mode"
+            className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+            aria-label="Dark"
             value="black"
+            onChange={handleThemeChange}
+            checked={theme === "black"}
           />
         </li>
         <li>
           <input
             type="radio"
             name="theme-dropdown"
-            className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-end"
-            aria-label="Rana's mode"
+            className="theme-controller w-full btn btn-sm btn-block btn-ghost justify-start"
+            aria-label="Rana"
             value="rana"
+            onChange={handleThemeChange}
+            checked={theme === "rana"}
           />
         </li>
       </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   plugins: [require("daisyui")],
   daisyui: {
     themes: [
-      "lemonade",
+      "light",
       "black",
       "rana"
     ],
