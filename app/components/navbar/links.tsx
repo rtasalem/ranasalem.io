b53a2155499e33962400d981333bc6974e8e1f3a
Rana Salem <ranatasalem@gmail.com>
2025-06-01 17:14:37 +0100

    wip: /experience - timeline

--- app/components/navbar/links.tsx
+++ app/components/navbar/links.tsx
@@ -1,7 +1,7 @@
 export function NavLinks() {
   return (
     <ul className="menu menu-horizontal px-1">
-      <li><a href="/about">About</a></li>
+      <li><a href="/experience">Experience</a></li>
       <li><a href="/blog">Blog</a></li>
       <li><a href="/projects">Projects</a></li>
       <li><a href="/connect">Connect</a></li>
---
+++ app/experience/page.tsx
@@ -0,0 +1,135 @@
+export default function About() {
+  return (
+    <div className="bg-base-200 text-base-content min-h-screen p-6">
+      <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+        <li>
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end">
+            <time className="font-mono italic">JAN 2024 - present</time>
+            <div className="text-lg font-black">Department for Environment, Food and Rural Affairs (Defra), Software Developer</div>
+            A 2-year placement in the UK Civil Service as a software developer specialising in Node.js.
+            Chosen to work within Defra's Farming and Countryside Programme as part of a multidisciplinary team 
+            building an online portal to improve online interactions between farmers and Defra.
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="timeline-end md:mb-10">
+            <time className="font-mono italic">APR 2023 - present</time>
+            <div className="text-lg font-black">FDM Group, IT Consultant</div>
+            iMac is a family of all-in-one Mac desktop computers designed and built by Apple Inc. It has
+            been the primary part of Apple's consumer desktop offerings since its debut in August 1998,
+            and has evolved through seven distinct forms
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end">
+            <time className="font-mono italic">2001</time>
+            <div className="text-lg font-black">iPod</div>
+            The iPod is a discontinued series of portable media players and multi-purpose mobile devices
+            designed and marketed by Apple Inc. The first version was released on October 23, 2001, about
+            8+1⁄2 months after the Macintosh version of iTunes was released. Apple sold an estimated 450
+            million iPod products as of 2022. Apple discontinued the iPod product line on May 10, 2022. At
+            over 20 years, the iPod brand is the oldest to be discontinued by Apple
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="timeline-end md:mb-10">
+            <time className="font-mono italic">2007</time>
+            <div className="text-lg font-black">iPhone</div>
+            iPhone is a line of smartphones produced by Apple Inc. that use Apple's own iOS mobile
+            operating system. The first-generation iPhone was announced by then-Apple CEO Steve Jobs on
+            January 9, 2007. Since then, Apple has annually released new iPhone models and iOS updates. As
+            of November 1, 2018, more than 2.2 billion iPhones had been sold. As of 2022, the iPhone
+            accounts for 15.6% of global smartphone market share
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end">
+            <time className="font-mono italic">2015</time>
+            <div className="text-lg font-black">Apple Watch</div>
+            The Apple Watch is a line of smartwatches produced by Apple Inc. It incorporates fitness
+            tracking, health-oriented capabilities, and wireless telecommunication, and integrates with
+            iOS and other Apple products and services
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
