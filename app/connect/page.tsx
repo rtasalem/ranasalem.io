08c2b57631ffe31a8223a7e1a6ece0aeb38a9067
Rana Salem <ranatasalem@gmail.com>
2025-06-04 00:58:10 +0100

    oos: start building connect + home page

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -1,7 +1,10 @@
 export default function Connect() {
   return (
-    <div>
-      <h1>connect</h1>
+    <div className="flex items-center justify-center bg-base-200 min-h-screen text-base-content">
+      <button className="btn btn-xl">GitHub</button>
+      <button className="btn btn-xl">LinkedIn</button>
+      <button className="btn btn-xl">Email</button>
+      <button className="btn btn-xl">ORCID</button>
     </div>
   )
 }
