99f960e526549178ae02319a53aa216538989daf
Rana Salem <ranatasalem@gmail.com>
2025-06-04 00:23:51 +0100

    update timeline

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -1,143 +1,145 @@
 export default function About() {
   return (
-    <div className="bg-base-200 text-base-content min-h-screen p-6">
-      <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-        <li>
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-          <div className="timeline-start mb-10 md:text-end">
-            <time className="font-mono italic">JAN 2024 - present</time>
-            <div className="text-lg font-black">Department for Environment, Food and Rural Affairs (Defra)</div>
-            <div className="font-black">Software Developer</div>
-            A 2-year placement in the UK Civil Service as a software developer specialising in Node.js.
-            Chosen to work within Defra's Farming and Countryside Programme as part of a multidisciplinary team 
-            building an online portal to improve online interactions between farmers and Defra.
-          </div>
-          <hr />
-        </li>
-        <li>
-          <hr />
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-          <div className="timeline-end md:mb-10">
-            <time className="font-mono italic">APR 2023 - present</time>
-            <div className="text-lg font-black">FDM Group</div>
-            <div className="font-black">IT Consultant</div>
-            Nominated for Consultant of the Month & FDM Champion.
-            Awarded the Tech Industry Gold Foundation Credential in software development following 
-            14 weeks of intensive training through FDM’s She Lives Tech academy. Trusted to deliver successful 
-            internal proof of concept projects as a backend software developer by leveraging Agile and Scrum 
-            principles. On a 2-year placement as a Software Developer with Defra (Department for Environment, 
-            Food and Rural Affairs).
-          </div>
-          <hr />
-        </li>
-        <li>
-          <hr />
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-          <div className="timeline-start mb-10 md:text-end">
-            <time className="font-mono italic">OCT 2022 - APR 2023</time>
-            <div className="text-lg font-black">Glasgow Caledonian University</div>
-            Assisting in teaching of BSc and PhD students in the Department of Applied Sciences. 
-            Utilising previous experience and expertise to demonstrate to students the use of laboratory 
-            instrumentation. Overseeing the implementation of safe working practices to promote health and 
-            safety amongst students and staff in the laboratory.
-          </div>
-          <hr />
-        </li>
-        <li>
-          <hr />
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-          <div className="timeline-end md:mb-10">
-            <time className="font-mono italic">2007</time>
-            <div className="text-lg font-black">iPhone</div>
-            iPhone is a line of smartphones produced by Apple Inc. that use Apple's own iOS mobile
-            operating system. The first-generation iPhone was announced by then-Apple CEO Steve Jobs on
-            January 9, 2007. Since then, Apple has annually released new iPhone models and iOS updates. As
-            of November 1, 2018, more than 2.2 billion iPhones had been sold. As of 2022, the iPhone
-            accounts for 15.6% of global smartphone market share
-          </div>
-          <hr />
-        </li>
-        <li>
-          <hr />
-          <div className="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="h-5 w-5"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-          <div className="timeline-start mb-10 md:text-end">
-            <time className="font-mono italic">MAY - AUG 2022</time>
-            <div className="text-lg font-black">Royal Society of Chemistry (RSC) Research Enablement Scholarship</div>
-            A scholarship funded by the RSC's Research Enablement Grant (£10,000) was received for a project that got featured 
-            at the Microscience Microscopy Congress (MMC2023) in Manchester, England. The project aimed to examine the effects of 
-            bleaching methods used in textile conservation and cultural heritage on cotton fibers of different ages and conditions. 
-            AFM (tapping and contact mode), SEM, and fluorescence spectroscopy were employed to analyse the fibres' responses to the 
-            treatments. The main focus of the project was on micro- and nanoscale imaging of fibre cross-sections and spectroscopic 
-            analysis of the bleaching solvents before and after their application to the textiles. This experience provided valuable 
-            insights into textile conservation and advanced microscopy techniques.
-          </div>
-        </li>
-      </ul>
+    <div className="bg-base-200 text-base-content min-h-screen">
+      <div className="max-w-6xl mx-auto">
+        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+          <li>
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-start mb-10 md:text-end">
+              <time className="font-mono italic">JAN 2024 - present</time>
+              <div className="text-lg font-black">Department for Environment, Food and Rural Affairs (Defra)</div>
+              <div className="font-black">Software Developer</div>
+              A 2-year placement in the UK Civil Service as a software developer specialising in Node.js.
+              Chosen to work within Defra's Farming and Countryside Programme as part of a multidisciplinary team
+              building an online portal to improve online interactions between farmers and Defra.
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-end md:mb-10">
+              <time className="font-mono italic">APR 2023 - present</time>
+              <div className="text-lg font-black">FDM Group</div>
+              <div className="font-black">IT Consultant</div>
+              Nominated for Consultant of the Month & FDM Champion.
+              Awarded the Tech Industry Gold Foundation Credential in software development following
+              14 weeks of intensive training through FDM’s She Lives Tech academy. Trusted to deliver successful
+              internal proof of concept projects as a backend software developer by leveraging Agile and Scrum
+              principles. On a 2-year placement as a Software Developer with Defra (Department for Environment,
+              Food and Rural Affairs).
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-start mb-10 md:text-end">
+              <time className="font-mono italic">OCT 2022 - APR 2023</time>
+              <div className="text-lg font-black">Glasgow Caledonian University</div>
+              Assisting in teaching of BSc and PhD students in the Department of Applied Sciences.
+              Utilising previous experience and expertise to demonstrate to students the use of laboratory
+              instrumentation. Overseeing the implementation of safe working practices to promote health and
+              safety amongst students and staff in the laboratory.
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-end md:mb-10">
+              <time className="font-mono italic">2007</time>
+              <div className="text-lg font-black">iPhone</div>
+              iPhone is a line of smartphones produced by Apple Inc. that use Apple's own iOS mobile
+              operating system. The first-generation iPhone was announced by then-Apple CEO Steve Jobs on
+              January 9, 2007. Since then, Apple has annually released new iPhone models and iOS updates. As
+              of November 1, 2018, more than 2.2 billion iPhones had been sold. As of 2022, the iPhone
+              accounts for 15.6% of global smartphone market share
+            </div>
+            <hr />
+          </li>
+          <li>
+            <hr />
+            <div className="timeline-middle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
+            <div className="timeline-start mb-10 md:text-end">
+              <time className="font-mono italic">MAY - AUG 2022</time>
+              <div className="text-lg font-black">Royal Society of Chemistry (RSC) Research Enablement Scholarship</div>
+              A scholarship funded by the RSC's Research Enablement Grant (£10,000) was received for a project that got featured
+              at the Microscience Microscopy Congress (MMC2023) in Manchester, England. The project aimed to examine the effects of
+              bleaching methods used in textile conservation and cultural heritage on cotton fibers of different ages and conditions.
+              AFM (tapping and contact mode), SEM, and fluorescence spectroscopy were employed to analyse the fibres' responses to the
+              treatments. The main focus of the project was on micro- and nanoscale imaging of fibre cross-sections and spectroscopic
+              analysis of the bleaching solvents before and after their application to the textiles. This experience provided valuable
+              insights into textile conservation and advanced microscopy techniques.
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
