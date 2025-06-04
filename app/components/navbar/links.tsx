2f587ecc7f45c45a4a811b900744269a13e1bbc8
Rana Salem <ranatasalem@gmail.com>
2025-06-04 17:26:38 +0100

    wip

--- app/components/navbar/links.tsx
+++ app/components/navbar/links.tsx
@@ -2,7 +2,7 @@ import { NavDropdown } from "./dropdown"
 
 export function NavLinks() {
   return (
-    <ul className="menu menu-horizontal dropdown-content px-1">
+    <ul className="menu menu-horizontal px-1">
       <li><a href="/blog">Blog</a></li>
       <li><a href="/experience">Experience</a></li>
       <li><a href="/projects">Projects</a></li>
