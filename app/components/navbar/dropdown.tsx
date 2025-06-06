d6e672087910affb04bc1014d9d8686eff0797f2
Rana Salem <ranatasalem@gmail.com>
2025-06-06 01:41:16 +0100

    add new theme

--- app/components/navbar/dropdown.tsx
+++ app/components/navbar/dropdown.tsx
@@ -2,7 +2,7 @@ export function NavDropdown() {
   return (
     <details>
       <summary>Connect</summary>
-      <ul className="bg-base-100 rounded-t-none p-2">
+      <ul className="bg-base-200 rounded-t-none p-2">
         <li><a href="mailto:ranatasalem@gmail.com">Email</a></li>
         <li><a href="https://github.com/rtasalem">GitHub</a></li>
         <li><a href="www.linkedin.com/in/ranatasalem">LinkedIn</a></li>
--- app/components/navbar/links.tsx
+++ app/components/navbar/links.tsx
@@ -3,9 +3,9 @@ import { NavDropdown } from "./dropdown"
 export function NavLinks() {
   return (
     <ul className="menu menu-horizontal px-1">
-      <li><a href="/blog">Blog</a></li>
       <li><a href="/experience">Experience</a></li>
       <li><a href="/projects">Projects</a></li>
+      <li><a href="/blog">Blog</a></li>
       <li><NavDropdown /></li>
     </ul>
   )
