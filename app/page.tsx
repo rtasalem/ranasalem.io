8b758645ffb55064deb82fee61c8e68e492d9f0b
Rana Salem <ranatasalem@gmail.com>
2025-06-15 21:33:28 +0100

    udpate home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,14 @@ export default function Home() {
     <div className="hero bg-base-100 min-h-160 text-base-content">
       <div className="hero-content text-center">
         <div>
-          <h1 className="text-9xl font-bold mb-6">RANA SALEM</h1>
-          <KbdKeys />
-          <p className="py-6">
+          <h1 className="text-9xl font-bold mb-3">RANA SALEM</h1>
+          <div className="mb-5">
+            <KbdKeys />
+          </div>
+          <p className="mb-5">
             Full-stack codesmith who occasionally writes about writing code.
           </p>
-          <div className="avatar">
+          <div className="avatar mb-5">
             <div className="w-100 rounded">
               <img src="avatar.png" alt="Avatar" />
             </div>
