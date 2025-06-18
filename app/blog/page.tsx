7ef92ab2bc46b5913e01e22478bbb694dd2b3f5c
Rana Salem <ranatasalem@gmail.com>
2025-06-18 22:24:26 +0100

    wip: blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -15,16 +15,27 @@ import { allPosts, Post } from 'contentlayer/generated'
 
 function PostCard(post: Post) {
   return (
+    // <div className="mb-8">
+    //   <h2 className="mb-1 text-xl">
+    //     <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
+    //       {post.title}
+    //     </Link>
+    //   </h2>
+    //   <time dateTime={post.date} className="mb-2 block text-xl text-gray-600">
+    //     {format(parseISO(post.date), 'LLLL d, yyyy')}
+    //   </time>
+    // </div>
     <div className="mb-8">
-      <h2 className="mb-1 text-xl">
-        <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
-          {post.title}
-        </Link>
-      </h2>
-      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
-        {format(parseISO(post.date), 'LLLL d, yyyy')}
-      </time>
-      <div className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0" dangerouslySetInnerHTML={{ __html: post.body.html }} />
+      <div className="flex items-center justify-between">
+        <h2 className="text-xl font-semibold mb-5 mt-10">
+          <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
+            {post.title}
+          </Link>
+        </h2>
+        <time dateTime={post.date} className="text-xl text-gray-500 whitespace-nowrap ml-4">
+          {format(parseISO(post.date), 'LLLL d, yyyy')}
+        </time>
+      </div>
     </div>
   )
 }
@@ -33,7 +44,7 @@ export default function Home() {
   const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
 
   return (
-    <div className="overflow-x-auto mx-auto max-w-xl bg-base-100 text-base-content">
+    <div className="overflow-x-auto mx-auto max-w-xl text-base-content bg-base-100">
       <div className='text-center'>
         <h1 className="text-3xl font-bold mt-5 mb-3">blog</h1>
         <p className="mb-5">A collection of all my writing...</p>
