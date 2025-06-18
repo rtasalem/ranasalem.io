11cb745ed3e1ee6c0db434d0407a6ff417dccb80
Rana Salem <ranatasalem@gmail.com>
2025-06-18 21:39:26 +0100

    wip: set up contentlayer2

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ export default function Blog() {
     <div className="overflow-x-auto bg-base-100 text-base-content">
       <div className="text-center">
         <h1 className="text-3xl font-bold mt-5 mb-3">blog</h1>
-        <p className="mb-5">A collection of all my writing that I publish on this little corner of the internet that I call mine...</p>
+        <p className="mb-5">A collection of all my writing...</p>
       </div>
     </div>
   )
