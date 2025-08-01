ae2d887440d3731b0bf6f5109551a1d156644d5b
Rana Salem <ranatasalem@gmail.com>
2025-08-01 21:33:01 +0100

    convert experience to about

--- app/components/navbar/links.tsx
+++ app/components/navbar/links.tsx
@@ -3,7 +3,7 @@ import { NavDropdown } from "./dropdown/dropdown"
 export function NavLinks() {
   return (
     <ul className="menu menu-horizontal">
-      <li><a href="/experience">experience</a></li>
+      <li><a href="/about">about</a></li>
       <li><a href="/projects">projects</a></li>
       <li><a href="Rana_Salem_CV.pdf" download="Rana_Salem_CV.pdf" target="_blank">cv</a></li>
       <li><a href="/blog">blog</a></li>
--- app/page.tsx
+++ app/page.tsx
@@ -5,32 +5,13 @@ export default function Home() {
     <div className="hero min-h-screen bg-base-100 text-base-content">
       <div className="hero-content text-center">
         <div>
-          <h1 className="text-9xl font-bold mb-3">RANA SALEM</h1>
-          <div className="mb-">
+          <h1 className="text-9xl font-bold mb-5">RANA SALEM</h1>
+          <div className="mb-5">
             <KbdKeys />
           </div>
           <p className="mb-5">
             Full-stack codesmith who occasionally writes about writing code.
           </p>
-          <div className="photos">
-            <div className="flex justify-center mb-2">
-              <div className="w-auto rounded">
-                <img
-                  src="mini-me-1.jpeg"
-                  alt="A childhood photo of Rana sat typing at a computer."
-                  className="w-150 h-110"
-                />
-              </div>
-              <div className="w-auto rounded">
-                <img
-                  src="mini-me-2.jpeg"
-                  alt="A childhood photo of Rana sat typing at a computer."
-                  className="w-150 h-110"
-                />
-              </div>
-            </div>
-            <p className="text-center italic">Photographs my parents took of me developing this website.</p>
-          </div>
         </div>
       </div>
     </div>
