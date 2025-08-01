47ab74a200146801f19a593ab2cf9e1028f04e97
Rana Salem <ranatasalem@gmail.com>
2025-08-01 20:21:56 +0100

    add photos of my to start page

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,31 @@ export default function Home() {
       <div className="hero-content text-center">
         <div>
           <h1 className="text-9xl font-bold mb-3">RANA SALEM</h1>
-          <div className="mb-5">
+          <div className="mb-">
             <KbdKeys />
           </div>
           <p className="mb-5">
             Full-stack codesmith who occasionally writes about writing code.
           </p>
-          {/* <div className="avatar mb-5">
-            <div className="w-100 rounded">
-              <img src="avatar.png" alt="Avatar" />
+          <div className="photos">
+            <div className="flex justify-center mb-2">
+              <div className="w-auto rounded">
+                <img
+                  src="mini-me-1.jpeg"
+                  alt="A childhood photo of Rana sat typing at a computer."
+                  className="w-150 h-110"
+                />
+              </div>
+              <div className="w-auto rounded">
+                <img
+                  src="mini-me-2.jpeg"
+                  alt="A childhood photo of Rana sat typing at a computer."
+                  className="w-150 h-110"
+                />
+              </div>
             </div>
-          </div> */}
+            <p className="text-center italic">Photographs my parents took of me developing this website.</p>
+          </div>
         </div>
       </div>
     </div>
