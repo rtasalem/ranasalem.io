3c459da60af8f4781a0f76275a2af44c51cb0eb0
Rana Salem <ranatasalem@gmail.com>
2025-08-08 16:03:33 +0100

    update blog layout

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,15 +4,15 @@ import { allPosts, Post } from 'contentlayer/generated'
 
 function PostCard(post: Post) {
   return (
-    <div className="mb-8">
-      <div className="flex items-center justify-between">
-        <h2 className="text-xl font-semibold mb-5 mt-10">
-          <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
+    <div>
+      <div className="flex items-center justify-between mb-5 mt-5">
+        <h2 className="text-xl">
+          <Link href={post.url} className="text-base-content hover:text-base-300 border-b border-transparent hover:border-base-300 pb-0.5">
             {post.title}
           </Link>
         </h2>
-        <time dateTime={post.date} className="text-l text-gray-500 whitespace-nowrap ml-4">
-          {format(parseISO(post.date), 'LLLL d, yyyy')}
+        <time dateTime={post.date} className="text-sm text-base-content whitespace-nowrap ml-4">
+          {format(parseISO(post.date), 'd LLL yyyy')}
         </time>
       </div>
     </div>
@@ -26,11 +26,13 @@ export default function Blog() {
     <div className="overflow-x-auto mx-auto max-w-xl text-base-content bg-base-100">
       <div className='text-center'>
         <h1 className="text-3xl font-bold mt-5 mb-3">Blog</h1>
-        <p className="mb-5">A collection of all my writing...</p>
+        <p className="mb-10">A collection of all my writing...</p>
       </div>
-      {posts.map((post, idx) => (
-        <PostCard key={idx} {...post} />
-      ))}
+      <div className="divide-y divide-base-300">
+        {posts.map((post, idx) => (
+          <PostCard key={idx} {...post} />
+        ))}
+      </div>
     </div>
   )
 }
