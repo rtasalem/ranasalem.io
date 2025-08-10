27457dae19a7a03bf5332ba82f73b3a7a6e9f351
Rana Salem <ranatasalem@gmail.com>
2025-08-10 18:45:30 +0100

    trimming the fat: /gear

--- app/gear/page.tsx
+++ app/gear/page.tsx
@@ -2,10 +2,10 @@ import { gearItems } from "../constants/gear-items"
 
 export default function Gear() {
   return (
-    <div className="overflow-x-auto bg-base-100 text-base-content">
+    <div className="overflow-auto space-y-5">
       <div className="text-center">
-        <h1 className="text-3xl font-bold mt-5 mb-3">Gear</h1>
-        <p className="mb-5">A table of everything that I currently use day-to-day working in tech<sup>1</sup>...</p>
+        <h1 className="text-3xl font-bold mt-5 mb-5">Gear</h1>
+        <p>A table of everything that I currently use day-to-day working in tech<sup>1</sup>...</p>
       </div>
       <div className="max-w-5xl mx-auto">
         <table className="table">
@@ -28,12 +28,10 @@ export default function Gear() {
             ))}
           </tbody>
         </table>
-        <div className="text-left mt-10 mb-15">
-          <p className="mb-3"><sup>1</sup>Disclaimer: Yes, I do <strong>love</strong> Logitech. Their bluetooth 
-          functionality for switching between devices (specifically on keyboards) is 
+        <p className="text-left text-xs mt-10 mb-3"><sup>1</sup>Disclaimer: Yes, I do <strong>love</strong> Logitech. Their bluetooth
+          functionality for switching between devices (specifically on keyboards) is
           second to none. That being said my opinions are all my own and I'm not (sadly)
           sponsored by Logitech (maybe one day 🤞🏽).</p>
-        </div>
       </div>
     </div>
   )
