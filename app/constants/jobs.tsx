aec9a6b63842f7df926e9c0393e9fe9125773b40
Rana Salem <ranatasalem@gmail.com>
2025-08-10 13:11:56 +0100

    trimming the fat: /about

--- app/constants/jobs.tsx
+++ app/constants/jobs.tsx
@@ -2,31 +2,37 @@ export const jobs = [
   { 
     date: 'January 2024 - present', 
     role: 'Software Developer (Node.js)', 
-    company: 'Department for Environment, Food and Rural Affairs (Defra)'
+    company: 'Department for Environment, Food and Rural Affairs (Defra)',
+    summary: '2-year UK Civil Service contract via FDM Group as a Node.js developer, working within Defra\'s Farming and Countryside Programme on a multidisciplinary team. Contributed to the development and maintenance of a large-scale online portal improving interactions between Defra and British farmers, supporting their essential work. Nominated as FDM\'s Consultant of the Month within two months of starting.'
   },
   {
     date: 'April 2023 - present',
     role: 'IT Consultant & Software Developer',
-    company: 'FDM Group'
+    company: 'FDM Group',
+    summary: 'Nominated for Consultant of the Month and FDM Champion for contributions made while working with Defra, one of FDM\'s public sector clients. Earned the Tech Industry Gold Foundation Credential in software development after 14 weeks of intensive training through FDM’s She Lives Tech academy. Delivered successful internal proof-of-concept projects as a backend software developer using Agile and Scrum. Created a full-stack, Barbie-themed e-commerce application as a final project, presented to internal FDM trainers.'
   },
   {
     date: 'October 2022 - April 2023',
     role: 'Laboratory Demonstrator',
-    company: 'Glasgow Caledonian University'
+    company: 'Glasgow Caledonian University',
+    summary: 'Teaching assistant and demonstrator facilitating laboratory sessions for final-year undergraduates and PhD students. Delivered interactive demonstrations on various laboratory equipment, including optical microscopy, Atomic Force Microscopy, and Scanning Electron Microscopy. Maintained high safety standards for staff and students by enforcing safe working practices, especially when handling corrosive or hazardous chemicals. Provided constructive feedback on student dissertations to help them submit their best possible work for final-year deadlines.'
   },
   {
     date: 'May - August 2022',
     role: 'Royal Society of Chemistry (RSC) Research Enablement Scholarship',
-    company: 'Glasgow Caledonian University & RSC'
+    company: 'Glasgow Caledonian University & RSC',
+    summary: 'Conducted intensive research on the effects of established textile bleaching protocols on the integrity of historic textile artefacts, specifically cotton fibres. Presented findings as a poster and flash talk at the Microscience Microscopy Congress (MMC2023) in Manchester, England. Utilised advanced tools, including Atomic Force Microscopy, Scanning Electron Microscopy, and fluorescence spectroscopy, to collect micro- and nano-scale imaging data.'
   },
   {
     date: 'July - September 2021',
     role: 'Postgraduate Research Intern',
-    company: 'Glasgow Caledonian University & University of Strathclyde'
+    company: 'Glasgow Caledonian University & University of Strathclyde',
+    summary: 'Conducted initial research into the effects of oxidative and reductive bleaching on historic cotton textile artefacts. Utilised advanced instrumentation, including Atomic Force Microscopy and Ultraviolet/Visible spectroscopy. Findings were published in the peer-reviewed, open-access Heritage Science journal.'
   },
   {
     date: 'June - August 2020',
     role: 'Royal Microscopical Society (RMS) Summer Studentship',
-    company: 'RMS'
+    company: 'RMS',
+    summary: 'Conducted initial research on the effects of oxidative and reductive bleaching on historic cotton textile artefacts. Used advanced instrumentation, including Atomic Force Microscopy and Ultraviolet/Visible spectroscopy. Findings were published in the peer-reviewed, open-access Heritage Science journal.'
   }
 ]
