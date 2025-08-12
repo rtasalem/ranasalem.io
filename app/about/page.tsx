2750fa198d7efb26454ef475c75fc239124aaa2e
Rana Salem <ranatasalem@gmail.com>
2025-08-12 22:55:13 +0100

    trimming the fat: /about pt. 3

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,13 +4,13 @@ import Timeline from "./components/timeline"
 
 export default function Experience() {
   return (
-    <div className="overflow-auto mx-auto max-w-6xl">
-      <div className="text-center space-y-5">
-        <h1 className="text-3xl font-bold mt-5">About</h1>
+    <div className="overflow-auto mx-auto max-w-6xl space-y-5">
+      <div className="text-center space-y-5 mt-5">
+        <h1 className="text-3xl">About</h1>
         <Bio />
         <Diff />
-        <h1 className="text-3xl font-bold">Timeline</h1>
-        <p className="mb-5">My career so far...</p>
+        <h1 className="text-3xl">Timeline</h1>
+        <p>My career so far...</p>
       </div>
       <Timeline />
     </div>
