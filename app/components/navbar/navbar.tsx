78ae18e9158c82d2daa132d0e99857d0290f240f
Rana Salem <ranatasalem@gmail.com>
2025-08-21 23:06:09 +0100

    remove commented code

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,20 +1,3 @@
-// import { NavbarBrand } from './brand'
-// import { NavLinks } from './links'
-
-// export default function Navbar() {
-//   return (
-//     <div className='navbar font-mono'>
-//       <div className='navbar-start'>
-//         <NavbarBrand />
-//       </div>
-//       <div className='navbar-center hidden lg:flex'>
-//         <NavLinks />
-//       </div>
-//       <div className='navbar-end'>
-//       </div>
-//     </div>
-//   )
-// }
 import { NavbarBrand } from './brand'
 import { NavLinks } from './links'
 
