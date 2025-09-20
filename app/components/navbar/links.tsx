31dfcdedf818756c6fc915eadef8347540b6eca1
Rana Salem <ranatasalem@gmail.com>
2025-09-20 06:13:11 +0100

    remove gear page

--- app/components/navbar/links.tsx
+++ app/components/navbar/links.tsx
@@ -7,7 +7,6 @@ export function NavLinks() {
       <li><Link href='/about'>About</Link></li>
       <li><Link href='/projects'>Projects</Link></li>
       <li><Link href='/RANA_SALEM_CV.pdf' download='Rana_Salem_CV.pdf' target='_blank'>CV</Link></li>
-      <li><Link href='/gear'>Gear</Link></li>
       <li><Link href='/blog'>Blog</Link></li>
       <li><NavDropdown /></li>
     </ul>
