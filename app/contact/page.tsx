79f03787e08264a696529677012f2c05e54ef014
Rana Salem <ranatasalem@gmail.com>
2025-10-14 16:35:54 +0100

    contact form: wip

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,15 @@
-export default function Contact () {
+export default function Contact() {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="overflow-auto mx-auto max-w-6xl space-y-5 mt-3 items-center">
+      <div>
+        <input type="text" placeholder="Name" className="input" />
+      </div>
+      <div>
+        <input type="text" placeholder="Email" className="input" />
+      </div>
+      <div>
+        <textarea className="textarea" placeholder="Message"></textarea>
+      </div>
     </div>
   )
 }
