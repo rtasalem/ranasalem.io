66d9e1e1fb729b2955bcc8809ecfff5c082a150e
Rana Salem <ranatasalem@gmail.com>
2025-10-14 14:45:24 +0100

    change url to say x over twitter

--- app/constants/dropdown-items.tsx
+++ app/constants/dropdown-items.tsx
@@ -9,7 +9,7 @@ import {
 export const dropdownItems = [
   { label: "Email", href: 'mailto:ranatasalem@gmail.com', icon: faEnvelope },
   { label: "GitHub", href: 'https://github.com/rtasalem', icon: faGithub },
-  { label: "X", href: 'https://twitter.com/rtasalem', icon: faXTwitter },
+  { label: "X", href: 'https://x.com/rtasalem', icon: faXTwitter },
   { label: 'LinkedIn', href: 'https://www.linkedin.com/in/ranatasalem/', icon: faLinkedin },
   { label: 'Orcid', href: "https://orcid.org/0000-0002-8683-561X", icon: faOrcid }
 ]
