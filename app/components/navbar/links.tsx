dcc6fe8900f7d05b7e7efda40b34705f44a6e52e
Rana Salem <ranatasalem@gmail.com>
2025-12-26 00:30:49 +0000

    update website

--- app/components/navbar/links.tsx
+++ app/components/navbar/links.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link'
 export function NavLinks() {
   return (
     <>
-      <li><Link href='/about'>About</Link></li>
       <li><Link href='/portfolio'>Portfolio</Link></li>
       <li><Link href='/blog'>Blog</Link></li>
+      <li><Link href='/timeline'>Timeline</Link></li>
       <li><Link href='/RANA_SALEM_CV.pdf' download='Rana_Salem_CV.pdf' target='_blank'>CV</Link></li>
       <li><Link href='/contact'>Contact</Link></li>
     </>
--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,16 +1,13 @@
-import Bio from '../components/about/bio'
-import Timeline from '../components/about/timeline'
+import CareerHistory from '../components/timeline/career-history'
 
 export default function Experience() {
   return (
     <div className='overflow-auto mx-auto max-w-6xl space-y-5'>
       <div className='text-center space-y-5 mt-5'>
-        <h1 className='text-3xl font-mono'>About</h1>
-        <Bio />
         <h1 className='text-3xl font-mono'>Timeline</h1>
         <p>My career so far.</p>
       </div>
-      <Timeline />
+      <CareerHistory />
     </div>
   )
 }
